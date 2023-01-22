68bcbad80a671c23a64ed64a5a83802258e21ba9
lawrencedulian <lawrenceedulian@gmail.com>
2023-01-23 00:47:55 +0100

    Add: Bonus !

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: "NotFound"
+}
+</script>
+
+<template>
+    <main>
+        <div class="container d-flex justify-content-center align-content-center" style="height: 80vh">
+            <h1 class="text-danger">404 | NOT FOUND</h1>
+        </div>
+    </main>
+</template>
