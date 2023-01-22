93fc2a3a28b74740e74f81ff6be4e69759228d21
lawrencedulian <lawrenceedulian@gmail.com>
2023-01-23 00:47:33 +0100

    Add: HomePage

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: "HomePage"
+}
+</script>
+
+<template>
+    <div class="container">
+        <h2 class="text-center mt-3">HomePage</h2>
+    </div>
+</template>
